67a34242e614123c316d3f5960f6bafc9a19c2bc
Mullins, Benjamin <benm@umich.edu>
2022-01-22 19:25:18 -0500

    first

---
+++ edit_this_file.js
@@ -0,0 +1,79 @@
+/**
+ * Problem 1: Assign the variable `thatBox` to 
+ * represent the element with id `thebox`
+ */
+
+/**
+ * Problem 2: Write the text of `thatBox` to 
+ * the console (using console.log)
+ */
+
+/**
+ * Problem 3: Add the class `color-change` to `thatBox`
+ */
+
+/**
+ * Problem 4: Add the class `red` to `thatBox`. 
+ * Problem 4.1: Using setInterval: 
+ * After 1 second, the `red` class should be removed and replaced with `blue`.
+ * This every-second red/blue exchange should continue indefinitely
+ */
+
+/**
+* Problem 5:
+* 
+* Create the variable `fruitListItems`. This should represent *all* 
+* list elements that are descendants of the element with id `fruit-list`
+*/
+
+/**
+ * Problem 6:
+ * 
+ * Iterate through `fruitListItems`, console log the text of each.
+ * 
+ */
+
+
+/**
+ * Problem 7:
+ * 
+ * Copy the iterator for `fruitListItems` in problem 6, 
+ * - remove the console.log
+ * - each `item` should have it's color changed to green
+ *   via an inline style.
+ *  
+ */
+
+/**
+ * Problem 8:
+ * 
+ * An array called `fruits` already eists
+ * Create the variable `fruitWithoutA` as an array that filters ouy the items 
+ * in `fruits` that include the lowercase letter 'a'.
+ * 
+ * Hint: while this can be done with loops, this *can* be done in
+ * a single (readable and well formatted) line. It's perfectly fine to do
+ * this in a loop if that's your preference, though
+ */
+
+
+/**
+ * Problem 9:
+ * Create a function `stopChanging` that cancels the interval created
+ * in problem 4.
+ */
+
+/**
+ * Problem 10:
+ * Create the variable `travelElement` that represents the element 
+ * with the id `traveler`.
+ * 
+ * Problem 10.1:
+ * Move `traveler` into the element with the id `destination`
+ * 
+ */
+
+/* 
+ * Problem 11: create a function `startTransitions` that adds
+ * the attribute `data-transitions` to the element with id `transition-box`
+*/
